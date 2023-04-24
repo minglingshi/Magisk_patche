2c82d751c581ae60579e8b98ba9f214c23910843
xyazzrany <jaxoncai3@gmail.com>
2023-04-24 18:58:38 +0800

    更新修补后的名字

--- time.sh
+++ time.sh
@@ -1,3 +1,3 @@
 current_time=$(date "+%Y%m%d_%H%M%S")
-mv done/new-boot.img boot_${current_time}.img
-mv boot_${current_time}.img done/
+mv done/new-boot.img Magisk_boot_${current_time}.img
+mv Magisk_boot_${current_time}.img done/
