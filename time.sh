4b1209cdcffbbc33b09e7e1108cad71d46b51837
xyazzrany <jaxoncai3@gmail.com>
2023-04-26 21:08:38 +0800

    修补时间改为中国时区时间

--- time.sh
+++ time.sh
@@ -1,3 +1,3 @@
-current_time=$(date "+%Y%m%d_%H%M%S")
+current_time=$(TZ='Asia/Shanghai' date "+%Y%m%d_%H%M%S")
 mv done/new-boot.img Magisk_boot_${current_time}.img
 mv Magisk_boot_${current_time}.img done/
