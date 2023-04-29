76fe9b7ecf41501ad994acc23cd68d8f1ad5aafb
xyazzrany <jaxoncai3@gmail.com>
2023-04-30 04:02:18 +0800

    update

--- boot.sh
+++ boot.sh
@@ -3,7 +3,7 @@
  ls *.img | while read i; do
         [[ "$i" = "$MODID" ]] && continue
         echo 正在修补$i
-        sh bin/boot_patch.sh $i
+        sh boot_patch.sh $i
         echo "$i is done"
         echo $i 完成 >>log.txt
         mv new-boot.img done/
